6ea62ceb86eb7a40ba0f9f70ea63dacfbe9455e0
Muhammad Iqbal CR <muh.iqbal.cr@gmail.com>
2024-05-25 01:22:19 -0500

    Partially finished adding parameters.

--- sigcap-to-csv.py
+++ sigcap-to-csv.py
@@ -1,19 +1,22 @@
-from argparse import ArgumentParser
+import argparse
+import csv
 from lib import loader
 from lib import filter_json
 from lib import util
 from lib import wifi_helper
+import logging
+from pathlib import Path
 
 max_lte = -1
 max_nr = -1
 max_wifi_2_4 = -1
 max_wifi_5 = -1
 max_wifi_6 = -1
+output_list = list()
 
 
 def cb_preprocess(obj):
-    print(f"Preprocessing... # of data: {len(obj['json'])}; \
-files: {','.join(obj['files'])}")
+    print(f"Processing... # of data: {len(obj['json'])}")
 
     sigcap = obj['json']
     options = obj['options']
@@ -55,14 +58,15 @@ files: {','.join(obj['files'])}")
 
 
 def cb_process(obj):
-    print(f"Processing... # of data: {len(obj['json'])}; \
-files: {','.join(obj['files'])}")
+    print(f"Processing... # of data: {len(obj['json'])}")
+    if len(obj["files"]) < 10:
+        print(f"Files: {','.join(obj['files'])}")
 
     sigcap = obj['json']
     options = obj['options']
-    print(options)
+    logging.info(options)
 
-    global max_lte, max_nr, max_wifi_2_4, max_wifi_5, max_wifi_6
+    global max_lte, max_nr, max_wifi_2_4, max_wifi_5, max_wifi_6, output_list
 
     # Compare maximum numbers with the requested maximum in the option
     if (options.max_lte is not None and options.max_lte < max_lte):
@@ -89,13 +93,47 @@ files: {','.join(obj['files'])}")
             and "carrierName" not in entry
         ):
             continue
-        # print(entry)
+
+        operator = util.get_operator_name(entry)
+        if (not options.include_invalid_op and operator == "Unknown"):
+            continue
+        temp_out = {
+            "sigcap_version": entry["version"],
+            "android_version": entry["androidVersion"],
+            "is_debug": entry["isDebug"],
+            "uuid": entry["uuid"],
+            "device_name": entry["deviceName"],
+            "timestamp": entry["datetimeIso"],
+            "latitude": entry["location"]["latitude"],
+            "longitude": entry["location"]["longitude"],
+            "altitude": entry["location"]["altitude"],
+            "hor_acc": entry["location"]["hor_acc"],
+            "ver_acc": entry["location"]["ver_acc"],
+            "operator": operator,
+            "network_type*": util.get_network_type(entry),
+            "override_network_type": entry["overrideNetworkType"],
+            "radio_type": entry["phoneType"],
+            "nrStatus": entry["nrStatus"],
+            "nrAvailable": entry["nrAvailable"],
+            "dcNrRestricted": entry["dcNrRestricted"],
+            "enDcAvailable": entry["enDcAvailable"],
+            "nrFrequencyRange": entry["nrFrequencyRange"],
+            "cellBandwidths": entry["cellBandwidths"],
+            "usingCA": entry["usingCA"],
+        }
+        if ("sensor" in entry and options.print_sensor_data):
+            for key, val in entry["sensor"].items():
+                temp_out[f"sensor.{key}"] = val
+
+        output_list.append(temp_out)
 
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument("input", help="input SigCap folder or file")
-    parser.add_argument("output", help="output CSV file with .csv suffix")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=Path,
+                        help="input SigCap folder or file")
+    parser.add_argument("output_file", type=argparse.FileType('w'),
+                        help="output CSV file with .csv suffix")
     parser.add_argument("--max-lte", type=int,
                         help="maximum number of LTE cells to be displayed")
     parser.add_argument("--max-nr", type=int,
@@ -104,15 +142,18 @@ def main():
                         help="maximum number of Wi-Fi APs to be displayed")
     parser.add_argument("--filter", type=str,
                         help="filter of JSON string or path to JSON file")
-    parser.add_argument("--include-invalid-op",
+    parser.add_argument("--include-invalid-op", action="store_true",
                         help="include invalid operator names")
-    parser.add_argument("--print-sensor-data", help="print out sensor data")
-    parser.add_argument("--print-ping", help="print out ping data")
-    parser.add_argument("--print-html-get", help="print out HTML GET data")
+    parser.add_argument("--print-sensor-data", action="store_true",
+                        help="print out sensor data")
+    parser.add_argument("--log-level", default="warning",
+                        help="Log level, default=warning")
     args = parser.parse_args()
+    logging.basicConfig(level=args.log_level.upper())
 
     if (args.filter is not None):
         args.filter = util.create_json_filter(args.filter)
+        print(f"Using filter: {args.filter}")
 
     print("===== Start preprocessing! =====")
     loader.load_json(args.input, cb_preprocess, options=args)
@@ -124,8 +165,62 @@ def main():
     print(f"Max number of Wi-Fi 5 GHz: {max_wifi_5}")
     print(f"Max number of Wi-Fi 6 GHz: {max_wifi_6}")
 
+    global output_list
+
     print("\n===== Start processing! =====")
     loader.load_json(args.input, cb_process, options=args)
+    output_list = sorted(output_list, key=lambda x: x["timestamp"])
+
+    print(f"Writing to {args.output_file.name} ...")
+    fieldnames = [
+        "sigcap_version",
+        "android_version",
+        "is_debug",
+        "uuid",
+        "device_name",
+        "timestamp",
+        "latitude",
+        "longitude",
+        "altitude",
+        "hor_acc",
+        "ver_acc",
+        "operator",
+        "network_type*",
+        "override_network_type",
+        "radio_type",
+        "nrStatus",
+        "nrAvailable",
+        "dcNrRestricted",
+        "enDcAvailable",
+        "nrFrequencyRange",
+        "cellBandwidths",
+        "usingCA",
+    ]
+    if (args.print_sensor_data):
+        fieldnames += [
+            "sensor.deviceTempC",
+            "sensor.ambientTempC",
+            "sensor.accelXMs2",
+            "sensor.accelYMs2",
+            "sensor.accelZMs2",
+            "sensor.battPresent",
+            "sensor.battStatus",
+            "sensor.battTechnology",
+            "sensor.battCapPerc",
+            "sensor.battTempC",
+            "sensor.battChargeUah",
+            "sensor.battVoltageMv",
+            "sensor.battCurrNowUa",
+            "sensor.battCurrAveUa",
+            "sensor.battEnergyNwh"
+        ]
+    csv_writer = csv.DictWriter(
+        args.output_file,
+        fieldnames=fieldnames)
+    csv_writer.writeheader()
+    csv_writer.writerows(output_list)
+
+    print(f"DONE!")
 
 
 if __name__ == "__main__":
